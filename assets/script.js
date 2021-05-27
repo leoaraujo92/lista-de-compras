dd8a28d0f2902367e494463477b16da4e5295825
leoaraujo92 <leonardo.araujo2008@gmail.com>
2021-05-27 20:20:07 -0300

    Adcionando os itens dinamicamente com o JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,76 @@
-let product = document.querySelector("#product-name")
-let price = document.querySelector("#product-price")
-let button = document.querySelector("#btn")
-let items = document.querySelectorAll(".products")
-let productName = document.querySelectorAll(".product")
-let priceItems = document.querySelectorAll(".price")
-let totalItems = document.querySelector(".total-items")
-let sumPriceItems = document.querySelector(".amount-items")
+const product = document.querySelector("#product-name")
+const price = document.querySelector("#product-price")
+const button = document.querySelector("#btn")
+const products = document.querySelector(".container-products")
+const productName = document.querySelectorAll(".product")
+const priceItems = document.querySelectorAll(".price")
+const totalItems = document.querySelector(".total-items")
+const sumTotalPrice = document.querySelector(".amount-items")
+const productsItems = [{
+    name: "Arroz integral",
+    price: 6.99
+}, {
+    name: "Feijão broto legal",
+    price: 9.99
+}, {
+    name: "Miojo de carne",
+    price: 1.80
+},{
+    name: "Agua mineral",
+    price: 1.55
+},{
+    name: "Peito de frango s/ osso 1kg",
+    price: 13.99
+}]
 
 
 
-function clearInputs(){
+
+productsItems.forEach(items => {
+    console.log(items.name, items.price)
+
+    let div = document.createElement('div')
+    div.classList.add('products')
+
+    div.innerHTML = `
+        
+        <div class="list flex-space shadow">
+            <div class="product medium-font">${items.name}</div>
+            <div class="price green-color medium-font">${items.price}</div>
+        </div>
+        <div class="del-item hand">X</div>
+        
+        `
+    products.append(div)
+})
+
+
+function clearInputs() {
     product.value = ""
     price.value = ""
 }
 
-function displayStatus(){
 
-    let totalSum = 0
-    for(let i = 0; i < priceItems.length; i++){
 
-        let stringToFloat = parseFloat(priceItems[i].innerHTML)
-        let priceItem = stringToFloat
+function displayStatus() {
 
-        totalSum = totalSum + priceItem
-    }
+    let totalPrice = 0
 
-    totalItems.innerHTML = `Total Items ${items.length}`
-    sumPriceItems.innerHTML = `Valor Total R$ ${totalSum.toFixed(2)} `
+    productsItems.forEach(priceItem => {
+
+        totalPrice = totalPrice + priceItem.price
+    })
+
+    totalItems.innerHTML = `Total Itens ${productsItems.length}`
+    sumTotalPrice.innerHTML = `Valor Total R$ ${totalPrice.toFixed(2)} `
 }
 
 
-button.addEventListener("click", function(){
-  
+
+button.addEventListener("click", function () {
+
     displayStatus()
     clearInputs()
-
 })
 
 
